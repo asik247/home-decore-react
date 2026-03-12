4180860bcd5189b1b64bc0b3df7bb72d5ce09e8b
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-12 23:24:56 +0600

    details page data show

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useLoaderData } from 'react-router';
 
 const Details = () => {
+    const matchData = useLoaderData()
+    console.log(matchData);
     return (
         <div>
             <h1>Iam Cards details pages</h1>
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -20,6 +20,14 @@ import Details from "../Pages/Details";
             // {path:'*',element:<h1>404 page not found</h1>}
             {
                 path:'/details/:id',
+                loader:async ({params})=>{
+                    const res = await fetch('/furnitureData.json')
+                    const data = await res.json()
+                    console.log(data);
+                    const singleData = data.find(single=>single.id===parseInt(params.id))
+                    // console.log(singleData);
+                    return singleData
+                },
                 Component:Details
             }
         ]
