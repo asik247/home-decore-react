a2587faea1c4d448d47a3db58638408dd578f8aa
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-12 15:51:10 +0600

    whistList pages setup

---
+++ src/Pages/WhisList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WhisList = () => {
+    return (
+        <div>
+            <h1>WhisList here</h1>
+        </div>
+    );
+};
+
+export default WhisList;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import Root from "../Root/Root";
 import Home from "../Pages/Home";
 import Products from "../Pages/Products";
+import WhisList from "../Pages/WhisList";
 
  const router = createBrowserRouter([
     {
@@ -9,7 +10,8 @@ import Products from "../Pages/Products";
         Component:Root,
         children:[
             {index:true,Component:Home},
-            {path:'products',Component:Products}
+            {path:'products',Component:Products},
+            {path:'whistList',Component:WhisList}
         ]
     }
 ])
