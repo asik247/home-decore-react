1abb3480c68e48251fa83e3888c5f9823fa0409d
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-12 16:34:23 +0600

    show error

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import { Link, useRouteError } from "react-router";
 
 const ErrorPage = () => {
-    return (
-        <div>
-            <h1>404 Page Not Found</h1>
-        </div>
-    );
+
+  const error = useRouteError();
+
+  return (
+    <div className="text-center mt-24">
+      <h1 className="text-6xl font-bold text-red-500">Oops!</h1>
+
+      {/* <p className="text-xl mt-4">Something went wrong</p> */}
+
+      <p className="text-gray-500 mt-2">
+        {error?.statusText || error?.message}
+      </p>
+
+      <Link to="/" className="btn btn-primary mt-6">
+        Go Back Home
+      </Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
