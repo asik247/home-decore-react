05956977cbaf06dc409210b42d87d9c352ac983c
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-12 17:59:15 +0600

    data show home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,11 +4,20 @@ import { useLoaderData } from 'react-router';
 const Home = () => {
     const allFurniture = useLoaderData();
     console.log(allFurniture);
-  
+    const furniture6 = allFurniture.slice(0, 6)
+    console.log(furniture6);
+
     return (
         <div>
             <h1>Iam home</h1>
-          
+            <div className='grid grid-cols-3 gap-4'>
+                {
+                    furniture6.map(item =>(<div>
+                        <h1>{item.name}</h1>
+                    </div>))
+                }
+            </div>
+
         </div>
     );
 };
