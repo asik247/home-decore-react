20e648fa6e31e6b843deea70362028e75f8bf3c3
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-12 15:48:31 +0600

    products pages implement

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,13 +1,15 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../Root/Root";
 import Home from "../Pages/Home";
+import Products from "../Pages/Products";
 
  const router = createBrowserRouter([
     {
         path:"/",
         Component:Root,
         children:[
-            {index:true,Component:Home}
+            {index:true,Component:Home},
+            {path:'products',Component:Products}
         ]
     }
 ])
