c14996c88fad46b6cc19a8ff75214c6071101dd7
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-13 09:17:07 +0600

    search almost done

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -5,6 +5,8 @@ const Product = ({products}) => {
     console.log(products);
     return (
         <div>
+            <h1 className='font-bold text-2xl mt-4'>All Products ({products.length})</h1>
+            
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {
                     products.map(p=>(
