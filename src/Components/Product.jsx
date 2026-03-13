1937ffbe243ff563419274901ffa0b0e876a1d3f
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-13 11:01:57 +0600

    add data local stored

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -13,9 +13,23 @@ const Product = ({products}) => {
     
     return (
         <div>
-            <div className='flex justify-between items-center'>
-                 <h1 className='font-bold text-2xl mt-4'>All Products ({searchValues.length})</h1>
-                 <input value={sarchValue} onChange={e=>setSearchValue(e.target.value)}  type="search" placeholder='Search' />
+             <div className="flex flex-col md:flex-row justify-between items-center mb-4 mt-6 gap-4">
+
+                <h1 className="text-3xl font-bold text-gray-800">
+                    All Products 
+                    <span className="text-blue-600 ml-2">
+                        ({searchValues.length}) is found
+                    </span>
+                </h1>
+
+                <input
+                    value={sarchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    type="search"
+                    placeholder="Search furniture..."
+                    className="w-full md:w-72 px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+
             </div>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
