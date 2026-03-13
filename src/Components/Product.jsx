f2244c41520fc58b991195bc70a9e28ec2d579aa
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-13 10:54:43 +0600

    search done

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -4,20 +4,23 @@ import SingleProduct from './SingleProduct';
 const Product = ({products}) => {
     // console.log(products);
     const [sarchValue,setSearchValue] = useState('');
-    console.log(sarchValue);
+    // console.log(sarchValue);
     const trims = sarchValue.trim().toLocaleLowerCase();
-    console.log(trims);
+    // console.log(trims);
+
+    const searchValues = trims? products.filter(product=>product.name.toLocaleLowerCase().includes(trims)):products
+    console.log(searchValues);
     
     return (
         <div>
             <div className='flex justify-between items-center'>
-                 <h1 className='font-bold text-2xl mt-4'>All Products ({products.length})</h1>
+                 <h1 className='font-bold text-2xl mt-4'>All Products ({searchValues.length})</h1>
                  <input value={sarchValue} onChange={e=>setSearchValue(e.target.value)}  type="search" placeholder='Search' />
             </div>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {
-                    products.map(p=>(
+                    searchValues.map(p=>(
                         <SingleProduct key={p.id} p={p}></SingleProduct>
                     ))
                 }
