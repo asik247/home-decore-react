2645ed2a2e97961cb821693424cb5a17c6cf380b
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-13 10:50:04 +0600

    all products filter

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -4,17 +4,15 @@ import SingleProduct from './SingleProduct';
 const Product = ({products}) => {
     // console.log(products);
     const [sarchValue,setSearchValue] = useState('');
-    const handler = (e)=>{
-        const searchV = e.target.value;
-        const searchVTrim = searchV.trim().toLocaleLowerCase()
-        console.log(searchVTrim);
-        setSearchValue(searchVTrim)
-    }
+    console.log(sarchValue);
+    const trims = sarchValue.trim().toLocaleLowerCase();
+    console.log(trims);
+    
     return (
         <div>
             <div className='flex justify-between items-center'>
                  <h1 className='font-bold text-2xl mt-4'>All Products ({products.length})</h1>
-                 <input value={sarchValue} onChange={handler} type="search" placeholder='Search' />
+                 <input value={sarchValue} onChange={e=>setSearchValue(e.target.value)}  type="search" placeholder='Search' />
             </div>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
