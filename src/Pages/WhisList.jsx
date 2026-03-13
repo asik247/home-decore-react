7c6dbde2bedf466b1134c667653287238c3fe6da
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-13 12:04:40 +0600

    show wishList Furniture done

--- src/Pages/WhisList.jsx
+++ src/Pages/WhisList.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router';
+import { getDB } from '../Utility/Utiltiy';
 
 const WhisList = () => {
     const allFurnitcre = useLoaderData();
-    console.log(allFurnitcre);
+    // console.log(allFurnitcre.id);
+    const [furniture,setFurniture] = useState([]);
+
+    useEffect(()=>{
+        const storedData = getDB();
+        // console.log(storedData);
+        const singleFurniture = allFurnitcre.filter(singleF=>storedData.includes(singleF.id));
+        setFurniture(singleFurniture);
+    },[])
     return (
         <div>
             <h1>WhisList here</h1>
+            <div>
+                {
+                    furniture.map(f=>(
+                        <div>
+                            <h1>{f.name}</h1>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 };
